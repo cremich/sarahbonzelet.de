dcaf025326f2e030d5fa540549de39611f9ae38a
Christian Bonzelet <christian.bonzelet@gmail.com>
2022-03-06 22:50:15 +0100

    fix: failing tests

--- packages/backend/test/website-stack.test.ts
+++ packages/backend/test/website-stack.test.ts
@@ -8,20 +8,6 @@ describe("Website stack", () => {
     app = new App();
   });
 
-  test("Resources are tagged with component tag", () => {
-    const stack = new WebsiteStack(app, "website", {});
-
-    const assert = assertions.Template.fromStack(stack);
-    assert.hasResourceProperties("AWS::CloudFormation::Stack", {
-      Tags: [
-        {
-          Key: "application",
-          Value: "sarahbonzelet.de",
-        },
-      ],
-    });
-  });
-
   test("Nested stack for contact form is created if target email address provided", () => {
     const stack = new WebsiteStack(app, "website", {
       contactFormRecipientEmailAddress: "me@example.com",

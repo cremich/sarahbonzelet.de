895c37d6c98b227fcf8a954b90ff85ccd886ae18
Christian Bonzelet <christian.bonzelet@gmail.com>
2022-03-18 22:08:12 +0100

    chore(backend): provide isolated and decentralized dev environment

--- packages/backend/bin/backend.ts
+++ packages/backend/bin/backend.ts
@@ -4,10 +4,14 @@ import * as cdk from "aws-cdk-lib";
 import { WebsiteStack } from "../lib/website-stack";
 import { config as uatProperties } from "../env/uat";
 import { config as prodProperties } from "../env/prod";
+import { config as devProperties } from "../env/dev";
 import { GithubStack } from "../lib/aws-account/github-stack";
 
 const app = new cdk.App();
 
+new WebsiteStack(app, "sarah-bonzelet-de-dev", {
+  ...devProperties,
+});
 new WebsiteStack(app, "sarah-bonzelet-de-uat", {
   ...uatProperties,
 });

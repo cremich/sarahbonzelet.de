0a31cdf5f565e7124cddb07a577e87bfd1f47e0f
Christian Bonzelet <christian.bonzelet@gmail.com>
2022-03-18 11:59:50 +0100

    Introduce UAT stage (#80)
    * chore: introduce uat stage
    
    * ci: remove uat branch from release workflow

--- packages/backend/bin/backend.ts
+++ packages/backend/bin/backend.ts
@@ -2,14 +2,14 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { WebsiteStack } from "../lib/website-stack";
-import { config as devProperties } from "../env/dev";
+import { config as uatProperties } from "../env/uat";
 import { config as prodProperties } from "../env/prod";
 import { GithubStack } from "../lib/aws-account/github-stack";
 
 const app = new cdk.App();
 
-new WebsiteStack(app, "sarah-bonzelet-de-dev", {
-  ...devProperties,
+new WebsiteStack(app, "sarah-bonzelet-de-uat", {
+  ...uatProperties,
 });
 new WebsiteStack(app, "sarah-bonzelet-de-prod", {
   ...prodProperties,
